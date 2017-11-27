f4f28a2689f428d7aed5b1aece5f4a2ff8985434
bpark <burt.parkers@googlemail.com>
2017-11-27 21:30:58 +0100

    removed animations and added x-axis description

--- src/app/stream-barchart/stream-barchart.component.ts
+++ src/app/stream-barchart/stream-barchart.component.ts
@@ -19,25 +19,29 @@ export class StreamBarchartComponent implements OnInit {
   countSubscription: Subscription;
   single: any[];
 
-  //  view: any[] = [700, 400];
+  view: number[] = undefined;
 
   // options
-  showXAxis = false;
+  animations = false;
+  showXAxis = true;
   showYAxis = true;
   gradient = false;
   showLegend = false;
   showXAxisLabel = false;
-  xAxisLabel = 'Country';
+  xAxisLabel = '';
   showYAxisLabel = false;
-  yAxisLabel = 'Population';
+  yAxisLabel = '';
+  tooltipDisabled = true;
+  barPadding = 8;
 
   colorScheme = {
     domain: ['#ea6100']
   };
 
   constructor(private serverSocket: ServerSocket) {
+    const now = Date.now();
     for (let i = 0; i < 100; i++) {
-      chartDataQueue.enqueue(new ChartData(0));
+      chartDataQueue.enqueue(new ChartData(0, this.formatXAxis(new Date(now - 101000 + i * 1000))));
     }
     Object.assign(this, {single});
 
@@ -45,9 +49,15 @@ export class StreamBarchartComponent implements OnInit {
 
   ngOnInit() {
     this.countSubscription = this.serverSocket.messages.bufferTime(1000).map(items => items.length).subscribe((count: number) => {
-      const data = chartDataQueue.enqueue(new ChartData(count));
+      const data = chartDataQueue.enqueue(new ChartData(count, this.formatXAxis(new Date())));
       this.single = [...data];
     });
   }
 
+  private formatXAxis(date: Date): string {
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return ('00' + minutes).slice(-2) + ':' + ('00' + seconds).slice(-2);
+  }
 }
